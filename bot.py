5ba4518433058166f6f3c9606b067a9ea79b754d
Abanob <abanobgerges11@yahoo.com>
2018-05-19 03:01:25 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-    await client.change_presence(game=discord.Game(name="Pes6Stars.cf"), status=discord.Status("online"))
+    await client.change_presence(game=discord.Game(name="Pes6Stars.cf | !server-status"), status=discord.Status("online"))
 
 @client.event
 async def on_message(message):
