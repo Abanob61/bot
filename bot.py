7a38a3f84ccef36405b9848920872ec7e38e8af4
Abanob <abanobgerges11@yahoo.com>
2018-05-19 02:58:49 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,13 +14,13 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+    await client.change_presence(game=discord.Game(name="Pes6Stars.cf"), status=discord.Status("online"))
 
 @client.event
 async def on_message(message):
     if message.content.startswith('!test'):
         counter = 0
         tmp = await client.send_message(message.channel, 'Calculating messages...')
-        await client.change_presence(game=discord.Game(name="Pes6Stars.cf"), status=discord.Status("online"))
         async for log in client.logs_from(message.channel, limit=100):
             if log.author == message.author:
                 counter += 1
