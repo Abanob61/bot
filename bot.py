11e80166914c328998c78d6ebbc467e39c529ffb
Abanob <abanobgerges11@yahoo.com>
2018-05-26 06:20:12 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -81,9 +81,9 @@ async def on_message(message):
            embed.add_field(name="Online Players", value=htmltext)
            await client.send_message(message.channel, embed=embed)  
     elif message.content.startswith('!rank'):
-		   message = message.content()
-		   profilename2 = re.split('\s+', message)[0][5:]
-		   profilename = message[len(profilename2) + 6:].strip()
+	   message = message.content()
+	   profilename2 = re.split('\s+', message)[0][5:]
+	   profilename = message[len(profilename2) + 6:].strip()
            url = requests.get("https://pes6stars.cf/adminususus/stats.php?p=Ahg377rx39x1&profile=.$profilename.")
            htmltext = url.text
            embed = discord.Embed(title="Pes6Stars Bot", description="Online players at Pes6stars server now.", color=0x00ff00)
