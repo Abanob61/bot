4e061a3b8cf0eb5975f0e88babc447cbd4fd540b
Abanob <abanobgerges11@yahoo.com>
2018-10-25 13:00:54 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,7 +37,7 @@ async def on_message(message):
            msg = message.content.strip()
            profilename = msg[7:].strip()
            print (profilename)
-           url = requests.get("http://80.211.157.169/adminususus/verify.php?p=statsdiscordbot125&profile=%s" % profilename)
+           url = requests.get("http://pes6stars.cf/adminususus/verify.php?p=statsdiscordbot125&profile=%s" % profilename)
            htmltext = url.text
            print (htmltext)
            if htmltext == "1":       
@@ -67,10 +67,10 @@ async def on_message(message):
         await client.send_message(message.channel, embed=embed)     
     elif message.content.startswith('!server-status'):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex(('80.211.157.169',20200))   
+        result = sock.connect_ex(('pes6stars.cf',20200))   
         sock.close()      
         if result == 0:
-           url = requests.get("http://80.211.157.169/onlineplayers.php")
+           url = requests.get("http://pes6stars.cf/onlineplayers.php")
            htmltext = url.text
            embed = discord.Embed(title="Pes6Stars Bot", description="Status of Pes6Stars server.", color=0x00ff00)
            embed.add_field(name="Lobbies Live!", value="[Lobbies List](<https://pes6stars.cf/lobbies.php>)")
@@ -83,25 +83,25 @@ async def on_message(message):
            embed.add_field(name="Author", value="Bob")
            embed.add_field(name="STATUS", value="OFFLINE")
            print ("Port is not open")  
-           embed.add_field(name="Lobbies Live!", value="[Lobbies List](<https://80.211.157.169/lobbies.php>)")  
+           embed.add_field(name="Lobbies Live!", value="[Lobbies List](<https://pes6stars.cf/lobbies.php>)")  
            await client.send_message(message.channel, embed=embed)
     elif message.content.startswith('!website-status') or message.content.startswith('!forum-status'):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex(('80.211.157.169',80))   
+        result = sock.connect_ex(('pes6stars.cf',80))   
         sock.close()      
         if result == 0:
            embed = discord.Embed(title="Pes6Stars Bot", description="Status of Pes6Stars site.", color=0x00ff00)
            embed.add_field(name="Author", value="Bob")
            embed.add_field(name="STATUS", value="ONLINE")
            print ("Port is open")  
-           embed.add_field(name="Lobbies Live!", value="[Lobbies List](<https://80.211.157.169/lobbies.php>)")  
+           embed.add_field(name="Lobbies Live!", value="[Lobbies List](<https://pes6stars.cf/lobbies.php>)")  
            await client.send_message(message.channel, embed=embed)
         else:
            embed = discord.Embed(title="Pes6Stars Bot", description="Status of Pes6Stars site.", color=0xff0000)
            embed.add_field(name="Author", value="Bob")
            embed.add_field(name="STATUS", value="OFFLINE")
            print ("Port is not open")  
-           embed.add_field(name="Lobbies Live!", value="[Lobbies List](<https://80.211.157.169/lobbies.php>)")  
+           embed.add_field(name="Lobbies Live!", value="[Lobbies List](<https://pes6stars.cf/lobbies.php>)")  
            await client.send_message(message.channel, embed=embed)   
     elif message.content.startswith('!onlineplayers') or message.content.startswith('!online-players') or message.content.startswith('!online-users') or message.content.startswith('!usersonline') or message.content.startswith('!onlineusers'):
            url = requests.get("http://pes6stars.cf/onlineplayers.php")
@@ -113,13 +113,13 @@ async def on_message(message):
            msg = message.content.strip()
            profilename = msg[6:].strip()
            print (profilename)
-           url = requests.get("http://80.211.157.169/adminususus/stats.php?p=statsdiscordbot125&profile=%s" % profilename)
+           url = requests.get("http://pes6stars.cf/adminususus/stats.php?p=statsdiscordbot125&profile=%s" % profilename)
            htmltext = url.text
            embed = discord.Embed(title="Pes6Stars Bot", description="Stats of your profile.", color=0x00ff00)
            embed.add_field(name="Stats", value=htmltext)
            await client.send_message(message.channel, embed=embed)           
     elif message.content.strip().startswith('!top1'):
-           url = requests.get("http://80.211.157.169/adminususus/stats.php?p=statsdiscordbot125&top1=list")
+           url = requests.get("http://pes6stars.cf/adminususus/stats.php?p=statsdiscordbot125&top1=list")
            htmltext = url.text
            embed = discord.Embed(title="Pes6Stars Bot", description="Top player rank #1 in Pes6Stars Server.", color=0x00ff00)
            embed.add_field(name="Top Player", value=htmltext)
